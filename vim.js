c8dbf4d2b5024e7dad189a145b9f093cb1d3062c
Purag Moumdjian <purmou@gmail.com>
2015-12-17 17:10:32 -0800

    added 'gg' functionality + better division of labor [vim object handles all command processing now]

--- vim.js
+++ vim.js
@@ -5,77 +5,96 @@
  * the 'n'/'@:' key to be pressed to repeat it.
  */
 var vim = new function () {
-	var input = document.querySelector(".vim input");
+  var input = document.querySelector(".vim input");
   var command = "";
-  
+  var last = 0;
+
   /* focusin on the command input -- the character entered appears
    * since we focusin on keydown events.
    */
   this.initCommand = function () {
     input.focus();
   };
-  
+
   /* clear the command input and defocus it */
   this.end = function () {
-  	input.value = "";
-  	input.blur();
+    input.value = "";
+    input.blur();
   };
-  
+
   /* reset the internal command state */
   this.reset = function () {
-  	command = "";
+    command = "";
     this.end();
   }
-  
-  /* parse and execute the inputted command */
-  this.processCommand = function () {
-  	/* extract the actual content of the command */
-  	var term = command.substr(1);
 
-  	switch (command[0]) {
-    	/* colon command */
-    	case ":":
-      	// do something here
-      	break;
+  /* parse and execute the inputted command */
+  this.processCommand = function (c) {
+    /* extract the actual content of the command */
+    var term = command.substr(1);
+    var resetLast = true;
+
+    switch (c ? c : command[0]) {
+      /* colon command */
+      case ":":
+        // do something here
+        break;
+        
       /* search command */
       case "/":
-      	// figure out how this works.
-      	window.find(term);
+        // figure out how this works.
+        window.find(term);
         break;
+        
+      /* 'G' - scroll to the bottom of the page */
+      case "G":
+      	window.scrollTo(0, document.body.scrollHeight);
+        break;
+        
+      /* 'gg' - scroll to the top of the page */
+      case "g":
+      	if (last == 71) {
+        	window.scrollTo(0, 0);
+        } else {
+  				last = 71;
+          resetLast = false;
+        }
+      	break;
     }
+    
+    if (resetLast) last = 0;
+    
     this.end();
   };
-  
+
   /* listen for keydowns on the input.
    * When enter is pressed, process the command
    */
-  input.addEventListener("keydown", function (e) {
-  	command = input.value;
+  input.addEventListener("keydown", function(e) {
+  	e.stopPropagation();
+    command = input.value;
     if (e.keyCode == 13) this.processCommand();
-  }.bind(this), true);
+  }.bind(this));
 }();
 
 /* Listen for key events on the window.
  * Process them and perform actions according to Vim key bindings
  */
 window.addEventListener("keydown", function (e) {
-	var k = e.keyCode;
+  var k = e.keyCode;
   if (e.shiftKey) {
-  	switch (k) {
-    	/* 'G' - Scroll to bottom of the page */
-    	case 71:
-      	window.scrollTo(0, document.body.scrollHeight);
-        break;
-      /* ':' - initiate command entry */
-      case 186:
-      	vim.initCommand();
-        break;
+    /* ':' - initiate command entry */
+    if (k == 186) {
+      vim.initCommand();
+    } else {
+      vim.processCommand(String.fromCharCode(k));
     }
   } else {
-  	switch (k) {
-    	/* '/' - initiate search command entry */
-    	case 191:
-      	vim.initCommand();
+    /* '/' - initiate search command entry */
+    if (k == 191) {
+    	vim.initCommand();
+    } else {
+      vim.processCommand(String.fromCharCode(k).toLowerCase());
     }
   }
-}, true);
+});
